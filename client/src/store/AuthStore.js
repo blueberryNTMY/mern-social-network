41b6f65e06e57758e040796c404614aca42d7574
blueberry <vlad.snouk@gmail.com>
2020-10-07 17:16:46 +0300

    the beginning of the integration of mobx

---
+++ client/src/store/AuthStore.js
@@ -0,0 +1,7 @@
+class AuthStore {
+  constructor(rootStore) {
+    this.rootStore = rootStore;
+  }
+}
+
+export default AuthStore;
---
+++ client/src/store/index.js
@@ -0,0 +1,13 @@
+import { createContext, useContext } from 'react';
+import RootStore from './RootStore';
+
+const rootStore = new RootStore();
+const RootStoreContext = createContext(rootStore);
+
+const RootStoreProvider = (props) => (
+  <RootStoreContext.Provider value={rootStore} {...props} />
+);
+
+const useRootStore = useContext(RootStoreContext);
+
+export { rootStore, RootStoreContext, RootStoreProvider, useRootStore };
